863fafc6bca35c9ceb34a2c4faad92e92761edce
Renato dos Santos Alves <renatosakasaki33@gmail.com>
2021-04-03 13:17:42 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,49 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            url: `https://gitlab.com/RenatoSakasaki/`,            
+            cart: 0,
+            selectedVariant: 0,
+
+            onSale: false,
+                     
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: [`pequeno`, 'medio, grande'],
+
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity:  0}
+            ]
+        }
+    },
+
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        remToCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        },       
+        updateVariant(index) {
+            this.selectedVariant = index
+        }      
+    },
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        }
+
+    }
+})
+
